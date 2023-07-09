465909016e880e55e177a7a85d7d07b050a59b84
Erin Boeger <erin.boeger@erlang-solutions.com>
2023-07-09 19:36:47 +0900

    addd tests for subtraction

--- lib/periods/computation.ex
+++ lib/periods/computation.ex
@@ -4,6 +4,15 @@ defmodule Periods.Computation do
   alias Periods.Period
 
   @index_units Enum.with_index(Periods.all_units())
+  @month_restrictions [:millisecond, :second, :minute, :hour, :week]
+
+  def add(%Period{unit: :month}, %Period{unit: unit}) when unit in @month_restrictions do
+    {:error, :invalid_month_addition}
+  end
+
+  def add(%Period{unit: unit}, %Period{unit: :month}) when unit in @month_restrictions do
+    {:error, :invalid_month_addition}
+  end
 
   def add(%Period{unit: unit} = period_1, %Period{unit: unit} = period_2) do
     total = period_1.amount + period_2.amount
@@ -13,6 +22,24 @@ defmodule Periods.Computation do
   def add(%Period{} = period_1, %Period{} = period_2) do
     unit = lowest_unit(period_1.unit, period_2.unit)
     add(convert(period_1, unit), convert(period_2, unit))
+  end
+
+  def subtract(%Period{unit: :month}, %Period{unit: unit}) when unit in @month_restrictions do
+    {:error, :invalid_month_subtraction}
+  end
+
+  def subtract(%Period{unit: unit}, %Period{unit: :month}) when unit in @month_restrictions do
+    {:error, :invalid_month_subtraction}
+  end
+
+  def subtract(%Period{unit: unit} = period_1, %Period{unit: unit} = period_2) do
+    total = period_1.amount - period_2.amount
+    %Period{amount: total, unit: unit}
+  end
+
+  def subtract(%Period{} = period_1, %Period{} = period_2) do
+    unit = lowest_unit(period_1.unit, period_2.unit)
+    subtract(convert(period_1, unit), convert(period_2, unit))
   end
 
   defp lowest_unit(unit_1, unit_2) do
--- test/computation_test.exs
+++ test/computation_test.exs
@@ -5,7 +5,7 @@ defmodule ComputationTest do
   alias Periods.Period
 
   describe "add/2" do
-    test "periods: with the same unit returns the total" do
+    test "with the same unit returns the total" do
       {:ok, period_1} = Periods.new(100)
       {:ok, period_2} = Periods.new(300)
       assert %Period{amount: 400, unit: :second} == Computation.add(period_1, period_2)
@@ -16,6 +16,64 @@ defmodule ComputationTest do
       {:ok, period_2} = Periods.new({300, :hour})
 
       assert %Period{amount: 540, unit: :hour} == Computation.add(period_1, period_2)
+    end
+
+    test "month addition restrictions returns error" do
+      for bad_unit <- [:millisecond, :second, :minute, :hour, :week] do
+        {:ok, period_1} = Periods.new({100, bad_unit})
+        {:ok, period_2} = Periods.new({300, :month})
+
+        assert {:error, :invalid_month_addition} == Computation.add(period_1, period_2)
+      end
+    end
+
+    test "adds months with years" do
+      {:ok, period_1} = Periods.new({2, :year})
+      {:ok, period_2} = Periods.new({10, :month})
+      assert %Period{amount: 34, unit: :month} == Computation.add(period_1, period_2)
+    end
+
+    test "adds months with decades" do
+      {:ok, period_1} = Periods.new({2, :decade})
+      {:ok, period_2} = Periods.new({10, :month})
+      assert %Period{amount: 250, unit: :month} == Computation.add(period_1, period_2)
+    end
+  end
+
+  describe "subtract/2" do
+    test "with the same unit returns the total" do
+      {:ok, period_1} = Periods.new(300)
+      {:ok, period_2} = Periods.new(100)
+
+      assert %Period{amount: 200, unit: :second} == Computation.subtract(period_1, period_2)
+    end
+
+    test "with the same unit allows a negative total" do
+      {:ok, period_1} = Periods.new(100)
+      {:ok, period_2} = Periods.new(400)
+
+      assert %Period{amount: -300, unit: :second} == Computation.subtract(period_1, period_2)
+    end
+
+    test "month subtraction restrictions returns error" do
+      for bad_unit <- [:millisecond, :second, :minute, :hour, :week] do
+        {:ok, period_1} = Periods.new({100, bad_unit})
+        {:ok, period_2} = Periods.new({300, :month})
+
+        assert {:error, :invalid_month_subtraction} == Computation.subtract(period_1, period_2)
+      end
+    end
+
+    test "subtracts months with years" do
+      {:ok, period_1} = Periods.new({2, :year})
+      {:ok, period_2} = Periods.new({10, :month})
+      assert %Period{amount: 14, unit: :month} == Computation.subtract(period_1, period_2)
+    end
+
+    test "subtracts months with decades" do
+      {:ok, period_1} = Periods.new({2, :decade})
+      {:ok, period_2} = Periods.new({10, :month})
+      assert %Period{amount: 230, unit: :month} == Computation.subtract(period_1, period_2)
     end
   end
 end
