3e58f67e207be657e388588e1dccf0d82570e3c4
Erin Boeger <erin.boeger@erlang-solutions.com>
2023-07-09 09:39:50 +0900

    have amount and basic new function

--- lib/periods/period.ex
+++ lib/periods/period.ex
@@ -8,7 +8,26 @@ defmodule Periods.Period do
   end
 
   def new(%{amount: amount, unit: unit}) when is_binary(amount) and unit in @units do
-    case parse_binary_amount(amount) do
+    case parse_amount(amount) do
+      {:ok, integer} -> new(%{amount: integer, unit: unit})
+      {:error, message} -> {:error, message}
+    end
+  end
+
+  def new(%{amount: _, unit: unit}) when unit in @units do
+    {:error, :amount_must_be_an_integer}
+  end
+
+  def new(%{amount: _, unit: unit}) when unit not in @units do
+    {:error, :bad_unit_type}
+  end
+
+  def new({amount, unit}) when is_integer(amount) and unit in @units do
+    new(%{amount: amount, unit: unit})
+  end
+
+  def new({amount, unit}) when is_binary(amount) and unit in @units do
+    case parse_amount(amount) do
       {:ok, integer} -> new(%{amount: integer, unit: unit})
       {:error, message} -> {:error, message}
     end
@@ -19,26 +38,22 @@ defmodule Periods.Period do
   end
 
   def new(amount) when is_binary(amount) do
-    case parse_binary_amount(amount) do
+    case parse_amount(amount) do
       {:ok, integer} -> new(%{amount: integer, unit: default_unit()})
       {:error, message} -> {:error, message}
     end
   end
 
-  def new({amount, unit}) when is_integer(amount) and unit in @units do
-    new(%{amount: amount, unit: unit})
-  end
-
-  def new(_), do: {:error, :cannot_parse}
+  def new(_amount), do: {:error, :amount_must_be_an_integer}
 
   defp default_unit do
     Application.get_env(Periods, :default_unit, :second)
   end
 
-  defp parse_binary_amount(amount) do
+  defp parse_amount(amount) do
     case Integer.parse(amount) do
       {integer, ""} -> {:ok, integer}
-      {_integer, remainder} when remainder != "" -> {:error, :amounts_must_be_integers}
+      {_integer, remainder} when remainder != "" -> {:error, :amount_must_be_an_integer}
       _ -> {:error, :cannot_parse_amount}
     end
   end
--- test/periods_test.exs
+++ test/periods_test.exs
@@ -1,8 +1,74 @@
 defmodule PeriodsTest do
-  use ExUnit.Case
-  doctest Periods
+  use ExUnit.Case,  async: false
+  # doctest Periods
 
-  test "greets the world" do
-    assert Periods.hello() == :world
+  alias Periods.Period
+
+  describe "new/1" do
+    test "map: with an integer amount and proper unit returns a struct" do
+      assert {:ok, %Period{amount: 100, unit: :day}} == Period.new(%{amount: 100, unit: :day})
+    end
+
+    test "map: with an binary amount and proper unit returns a struct" do
+      assert {:ok, %Period{amount: 100, unit: :day}} == Period.new(%{amount: "100", unit: :day})
+    end
+
+    test "map: with an invalid amount returns error" do
+      for bad_amount <- [1.23, "1.23", %{}, [], {}] do
+        {:error, message} = Period.new(%{amount: bad_amount, unit: :second})
+        assert message == :amount_must_be_an_integer
+      end
+    end
+
+    test "map: with invalid unit returns error" do
+      for bad_unit <- [2, 1.23, "decimal", :decimal, %{}, [], {}] do
+        {:error, message} = Period.new(%{amount: 1, unit: bad_unit})
+        assert message == :bad_unit_type
+      end
+    end
+
+    test "tuple: with an integer amount and proper unit returns a struct" do
+      assert {:ok, %Period{amount: 100, unit: :day}} == Period.new({100, :day})
+    end
+
+    test "tuple: with an binary amount and proper unit returns a struct" do
+      assert {:ok, %Period{amount: 100, unit: :day}} == Period.new({"100", :day})
+    end
+
+    test "tuple: with an invalid amount returns error" do
+      for bad_amount <- [1.23, "1.23", %{}, [], {}] do
+        {:error, message} = Period.new({bad_amount, :day})
+        assert message == :amount_must_be_an_integer
+      end
+    end
+
+    test "tuple: with invalid unit returns error" do
+      for bad_unit <- [2, 1.23, "decimal", :decimal, %{}, [], {}] do
+        {:error, message} = Period.new(%{amount: 1, unit: bad_unit})
+        assert message == :bad_unit_type
+      end
+    end
+
+    test "amount: with valid integer sets default unit" do
+      assert {:ok, %Period{amount: 100, unit: :second}} == Period.new(100)
+    end
+
+    test "amount: with binary integer sets default unit" do
+      assert {:ok, %Period{amount: 100, unit: :second}} == Period.new("100")
+    end
+
+    test "amount: with application env sets amount" do
+      Application.put_env(Periods, :default_unit, :day)
+      assert {:ok, %Period{amount: 100, unit: :day}} == Period.new(100)
+      Application.delete_env(Periods, :default_unit)
+    end
+
+    test "amount: with invalid amount returns error" do
+      for bad_amount <- [1.23, "1.23", %{}, [], {}] do
+        {:error, message} = Period.new(bad_amount)
+        assert message == :amount_must_be_an_integer
+      end
+    end
   end
+
 end
