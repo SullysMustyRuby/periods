f5eb23de39a6f295c76f69e5435993f0f01f13ed
Erin Boeger <erin.boeger@erlang-solutions.com>
2023-07-09 20:42:16 +0900

    added defdelegate for computation

--- lib/periods.ex
+++ lib/periods.ex
@@ -3,14 +3,19 @@ defmodule Periods do
   Documentation for `Periods`.
   """
 
+  alias Periods.Computation
   alias Periods.Conversion
   alias Periods.Parser
 
   @units [:millisecond, :second, :minute, :hour, :day, :week, :month, :year, :decade]
 
+  defdelegate add(value_1, value_2), to: Computation
+
   defdelegate convert(period, unit), to: Conversion
 
   defdelegate new(value), to: Parser
+
+  defdelegate subtract(value_1, value_2), to: Computation
 
   def all_units, do: @units
 end
